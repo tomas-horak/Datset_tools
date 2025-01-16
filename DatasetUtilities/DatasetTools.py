1c19cc3e4448f21abfafc7f394b9632026c67a59
tomikjeztracen <t.horako@email.cz>
2025-01-17 00:36:33 +0100

    refactoring

--- DatasetUtilities/DatasetTools.py
+++ DatasetUtilities/DatasetTools.py
@@ -1,30 +1,63 @@
+import logging
+import os
+
 from DatasetUtilities.tools_utils.dataset_distribution_tool import show_distribution_from_csv
 from DatasetUtilities.tools_utils.decomposer import Decomposer
 from DatasetUtilities.tools_utils.folders_to_dataset import ImageFoldersToDataset
 from DatasetUtilities.tools_utils.resizer import ImageResizer
 import h5py
 import numpy as np
+from pathlib import Path
 
 
 class DatasetTools:
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+
     @staticmethod
-    def decompose_dataset(dataset_path, csv_path, output_dir):
+    def decompose_dataset(path_to_dataset, path_to_csv, csv_column_names, output_dir, one_hot = False):
         """
         This utility is used for converting a single file with images and corresponding mapping in csv to a corresponding
         folder structure with images
 
-        :param dataset_path: folder where dataset is saved
-        :param csv_path: path to csv file with labels and ids
-        :param output_dir: path to where files with images should be created
+        :param path_to_dataset: folder where dataset is saved
+        :param path_to_csv: path to csv file with labels and ids
+        :param csv_column_names: tuple, containing names of columns in csv file. First value corresponds to names
+        of images, second value corresponds to name of column with target classes. eg: (filename, class)
+        :param output_dir: path to where files with images should be created, if it does not exist, it will be created
+        :param one_hot: If True, the CSV is expected to be one-hot encoded
+
         :return:
         """
 
-        decomposer = Decomposer(dataset_path=dataset_path, labels_csv_path=csv_path, output_directory=output_dir)
-        decomposer.process()
+        if not os.path.exists(path_to_dataset):
+            raise ValueError(f"Input directory {path_to_dataset} does not exist.")
+
+        if Path(path_to_csv).suffix != ".csv":
+            raise TypeError(f"CSV file {path_to_csv} does not exist or is in wrong format.")
+
+        if not os.path.exists(path_to_csv):
+            raise ValueError(f"CSV file {path_to_csv} does not exist.")
+
+        try:
+            decomposer = Decomposer(path_to_dataset, path_to_csv, output_dir, csv_column_names, one_hot, logger = DatasetTools.logger)
+            decomposer.process()
+            DatasetTools.logger.info("Decomposition completed.")
+        except Exception as e:
+            DatasetTools.logger.error(f"Error decomposing dataset: {e}")
 
     @staticmethod
-    def create_dataset_from_folders(folders_directory, output_path):
-        dataset_creator = ImageFoldersToDataset(path_to_folder_with_folders= folders_directory, output_path= output_path)
+    def create_dataset_from_folders(path_to_directory, output_path):
+        """
+        This utility is used for creating dataset from directory of folders.
+
+        :param path_to_directory: path to folder with folders corresponding to classes
+        :param output_path: directory, where dataset should be created
+        """
+        if not os.path.exists(path_to_directory):
+            raise ValueError(f"Input directory {path_to_directory} does not exist.")
+
+        dataset_creator = ImageFoldersToDataset(path_to_folder_with_folders= path_to_directory, output_path= output_path)
         dataset_creator.process()
 
     @staticmethod
--- DatasetUtilities/tools_utils/decomposer.py
+++ DatasetUtilities/tools_utils/decomposer.py
@@ -3,47 +3,64 @@ import numpy as np
 import os
 import shutil
 
+from keras.src.legacy.preprocessing.text import one_hot
 
-class Decomposer():
+
+class Decomposer:
     df_with_image_names_and_labels = pd.DataFrame()
 
-    def __init__(self, dataset_path, labels_csv_path, output_directory):
+    def __init__(self, dataset_path, labels_csv_path, output_directory, csv_names, one_hot, logger):
         self.dataset_path = dataset_path
         self.labels_csv_path = labels_csv_path
         self.output_directory = output_directory
         self.dataframe = pd.read_csv(self.labels_csv_path)
-
+        self.filename_column, self.classes_column = csv_names
+        self.one_hot = one_hot
+        self.logger = logger
 
     def classes_csv_to_df(self):
         labels = self.dataframe.columns
         data_list = []
-        print("skipped")
-
-        #for index, row in self.dataframe.iterrows():
-
-            #transposed = pd.DataFrame(row).transpose().to_numpy()
-            #image_name = transposed[0][0]
-            #print(image_name)
-            #print(np.where(transposed[0] == 1))
-            #index_of_category = np.where(transposed[0] == 1)[0][0]
-            #data_list.append({"filename": image_name, "category": labels[index_of_category]})
-        #self.df_with_image_names_and_labels = pd.DataFrame(data_list)
+        for index, row in self.dataframe.iterrows():
+            transposed = pd.DataFrame(row).transpose().to_numpy()
+            image_name = transposed[0][0]
+            index_of_category = np.where(transposed[0] == 1)[0][0]
+            data_list.append({self.filename_column: image_name, self.classes_column: labels[index_of_category]})
+        self.df_with_image_names_and_labels = pd.DataFrame(data_list)
 
     def create_target_folder(self):
         os.makedirs(self.output_directory, exist_ok=True)
 
     def create_folders_for_categories(self):
-        print(self.dataframe.columns)
-        labels = self.dataframe["category"].unique()
-        for label in labels:
-            class_folder = os.path.join(self.output_directory, str(label))
-            os.makedirs(class_folder, exist_ok=True)
+        def create_folders_from_list(labels):
+            for label in labels:
+                class_folder = os.path.join(self.output_directory, str(label))
+                os.makedirs(class_folder, exist_ok=True)
+
+        if self.one_hot:
+            labels = self.df_with_image_names_and_labels[self.classes_column].unique()
+            create_folders_from_list(labels)
+
+        else:
+            category_column = self.classes_column
+            labels = self.dataframe[category_column].unique()
+            create_folders_from_list(labels)
+
 
     def copy_files(self):
-        print("copying")
-        for _, item in self.dataframe.iterrows():
-            filename = item['filename'].strip()
-            category = item['category']
+
+        if one_hot:
+            df = self.df_with_image_names_and_labels
+        else:
+            df = self.dataframe
+
+        for _, item in df:
+            filename = self.filename_column
+            classes = self.classes_column
+
+
+            filename = item[filename].strip()
+            category = item[classes]
 
             source_path = os.path.join(self.dataset_path, filename)
             class_folder = os.path.join(self.output_directory, str(category))
@@ -58,11 +75,14 @@ class Decomposer():
                 print(f"Warning: {filename} not found in {self.dataset_path}")
 
     def process(self):
-        print("Processing labels CSV...")
-        self.classes_csv_to_df()
-        print("Creating category folders...")
+        self.create_target_folder()
+        if self.one_hot:
+            self.logger.info("One hot vectors converted")
+            self.classes_csv_to_df()
+
+        self.logger.info("Creating category folders...")
         self.create_folders_for_categories()
         print("Copying files into categorized folders...")
+        self.logger.info("Copying files into categorized folders...")
         self.copy_files()
-        print("Decomposition complete.")
 
